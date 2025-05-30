b1f331ce3d715d44f411aff13e3e7885abf3a212
Hemanth Kumar M <hemanthkumarrgukt@gmail.com>
2025-05-30 20:18:12 +0530

    fix deployment bug

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Enable CORS for all routes
 app.use(cors({
-  origin: ["http://localhost:5173","http://127.0.0.1:5173"],
+  origin: ["http://localhost:5173","http://127.0.0.1:5173", "https://evoltsoft-1.onrender.com"],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
